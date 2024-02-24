8dd60e2780c46d698aae781a415100b9f42613b0
lvsir <18346556974@139.com>
2024-02-25 04:04:09 +0800

    1.js

---
+++ 1.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2024-02-24
+// @description  try to take over the world!
+// @author       You
+// @match        https://greasyfork.org/zh-CN/script_versions/new
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
