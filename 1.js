3bf10eb651017e137a5e61132888099d8de406b5
lvsir <18346556974@139.com>
2024-02-25 04:11:23 +0800

    1.js

--- 1.js
+++ 1.js
@@ -1,16 +1,19 @@
 // ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
+// @name         hello world
+// @namespace    https://github.com/lvsir/youhou
 // @version      2024-02-24
 // @description  try to take over the world!
-// @author       You
-// @match        https://greasyfork.org/zh-CN/script_versions/new
+// @author       lvsir
+// @match        *://*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(
+    function() {
     'use strict';
+    console.log('hello world')
 
     // Your code here...
-})();
+})
+();
